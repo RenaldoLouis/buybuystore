de7db42a765fe3069e17c3a9a57ad9445beda47e
RenaldoLouis <renald@10xers.co>
2024-03-30 22:30:14 +0700

    set up date range

--- src/pages/carts.tsx
+++ src/pages/carts.tsx
@@ -9,16 +9,23 @@ import CartTable from "@/components/molecules/CartTable";
 import { cart, product } from "@/apis";
 import Button from '@mui/material/Button';
 import commonStyles from "@/styles/common.module.css";
+import { TuiDateRangePicker } from "nextjs-tui-date-range-picker";
+
 
 const Carts = () => {
     const [productsData, setProductsData] = useState([])
+    const [cartsData, setCartsData] = useState([])
+
+    const options = {
+        language: 'en',
+        format: 'MM-dd YYYY',
+    };
 
     const getAllProducts = () => {
         product.getAllProducts().then((res: any) => {
             setProductsData(res.data)
         })
     }
-    const [cartsData, setCartsData] = useState([])
 
     useEffect(() => {
         getAllProducts();
@@ -27,6 +34,9 @@ const Carts = () => {
         })
     }, [])
 
+    const handleChangeDate = () => {
+
+    }
 
     return (
         <LayoutHome>
@@ -34,10 +44,19 @@ const Carts = () => {
                 className={styles.homeContainer}
             >
                 <Suspense fallback={<div>Loading...</div>}>
-
-                    <Button className={commonStyles.marginY16} variant="contained">
-                        Add Cart
-                    </Button>
+                    <div className={commonStyles.flex} >
+                        <Button className={commonStyles.marginY16} variant="contained">
+                            Add Cart
+                        </Button>
+                        <TuiDateRangePicker
+                            handleChange={handleChangeDate}
+                            options={options}
+                            inputWidth={80}
+                            containerWidth={200}
+                            startpickerDate={new Date('2023-01-02')}
+                            endpickerDate={new Date('2023-01-30')}
+                        />
+                    </div>
                     <CartTable data={cartsData} productsData={productsData} />
                 </Suspense>
             </main>
